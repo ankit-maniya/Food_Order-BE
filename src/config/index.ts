5f4ee86f4abdadbbfbffedf5ce95b5f0afabf4a6
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-08-15 23:11:30 +0530

    ORIGNALE URI fixes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,3 @@
-export const MONGO_URI = "mongodb+srv://ankit:ankit@cluster0.sxl26.mongodb.net/onlinefoodapp?retryWrites=true&w=majority"
-// "mongodb://localhost:27017/online_food";
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/online_food";
 export const APP_SECRATE = "FOOD_ORDER_BE";
 export const PORT = process.env.PORT || 8000;
